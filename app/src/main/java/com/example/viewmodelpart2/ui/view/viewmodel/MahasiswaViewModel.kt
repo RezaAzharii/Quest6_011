523878f146941ce38c9c92d4ef7cea7f9c9181f5
RezaAzharii <rezaazhari867@gmail.com>
2024-11-22 22:37:39 +0700

    membuat package viewmodel dan MahasiswaViewModel dalam package viemodel

---
+++ app/src/main/java/com/example/viewmodelpart2/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.viewmodelpart2.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.viewmodelpart2.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel: ViewModel() {
+
+}

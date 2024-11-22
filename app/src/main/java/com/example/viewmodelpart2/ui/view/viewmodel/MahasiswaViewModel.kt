67884182916d572bfcfa35360c512bf54ab444f3
RezaAzharii <rezaazhari867@gmail.com>
2024-11-22 22:39:22 +0700

    membuat fungsi input pada MahasiswaViewModel

--- app/src/main/java/com/example/viewmodelpart2/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/viewmodelpart2/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -8,5 +8,26 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel: ViewModel() {
+    private val _mhsState = MutableStateFlow(Mahasiswa())
+    val mhsStateUi: StateFlow<Mahasiswa> = _mhsState.asStateFlow()
+
+    fun setNim(inputNim: String){
+        _mhsState.update {
+                stateNim -> stateNim.copy(nim = inputNim)
+        }
+    }
+
+    fun setNama(inputNama: String){
+        _mhsState.update {
+                stateNama -> stateNama.copy(nama = inputNama)
+        }
+    }
+
+    fun setEmail(inputEmail: String){
+        _mhsState.update {
+                stateEmail -> stateEmail.copy(email = inputEmail)
+        }
+    }
+
 
 }
